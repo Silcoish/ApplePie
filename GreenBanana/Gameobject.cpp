5b27e2d2b9e29e9c62f14f9a7662d107e5be97e9
CSchnitzerling <chris.schnitzerling@hotmail.com>
2015-05-19 15:21:20 +1000

    Build woking

--- GreenBanana/Gameobject.cpp
+++ GreenBanana/Gameobject.cpp
@@ -1,26 +1,6 @@
 #include "Gameobject.h"
 
-Gameobject::Gameobject()
-{
-}
 
-Gameobject::Gameobject(std::string type, std::string name, float x, float y, bool worldSpace, bool isStatic)
-{
-	SetType(type);
-	SetName(name);
-	SetPosition(sf::Vector2f(x,y));
-	SetworldSpace(worldSpace);
-	SetIsStatic(isStatic);
-}
-
-Gameobject::~Gameobject()
-{
-}
-
-void Gameobject::Update(float dt)
-{
-
-}
 
 void Gameobject::Render(sf::RenderWindow* window)
 {
--- GreenBanana/Player.cpp
+++ GreenBanana/Player.cpp
@@ -5,6 +5,15 @@ Gameobject::Gameobject()
 
 }
 
+Gameobject::Gameobject(std::string type, std::string name, float x, float y, bool worldSpace, bool isStatic)
+{
+	SetType(type);
+	SetName(name);
+	SetPosition(sf::Vector2f(x, y));
+	SetworldSpace(worldSpace);
+	SetIsStatic(isStatic);
+}
+
 Gameobject::~Gameobject()
 {
 
