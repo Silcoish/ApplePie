7346c48d475083ff983e1b7b218604a0dac23edc
CSchnitzerling <chris.schnitzerling@hotmail.com>
2015-05-21 12:17:11 +1000

    Health Instantiating

--- GreenBanana/Health.cpp
+++ GreenBanana/Health.cpp
@@ -8,27 +8,30 @@ Health::Health(std::string type, std::string name, float x, float y, bool worldS
 	SetworldSpace(worldSpace);
 	SetIsStatic(isStatic);
 	SetDepth(depth);
+	
 
 	SpritesheetLoader loader;
 
 	Animation healthFull;
-	healthFull.sprites = loader.Load("Resources/Objects/HealthObjects/sugar.png", 54, 336, 89);
-	healthFull.endEvent = Animation::AnimationEndEvent::HoldLastFrame;
-	healthFull.sprites[0]->sprite->setScale(sf::Vector2f((60 / 54), (340 / 336)));
-	healthFull.sprites[0]->width = 60;
-	healthFull.sprites[0]->height = 340;
+	healthFull.sprites = loader.Load("Resources/Objects/HealthObjects/sugar.png", 144, 124, 89);
+	healthFull.endEvent = Animation::AnimationEndEvent::Loop;
+	//healthFull.sprites[0]->sprite->setScale(sf::Vector2f((60 / 54), (340 / 336)));
+	healthFull.sprites[0]->width = 144;
+	healthFull.sprites[0]->height = 124;
+	healthFull.globalSpeed = 0.02;
 	animations.animations["healthFull"] = healthFull;
 
 	Animation healthEmpty;
-	healthEmpty.sprites = loader.Load("Resources/Objects/HealthObjects/sugar_Empty.png", 54, 336, 1);
+	healthEmpty.sprites = loader.Load("Resources/Objects/HealthObjects/sugar_Empty.png", 144, 124, 1);
 	healthEmpty.endEvent = Animation::AnimationEndEvent::HoldLastFrame;
-	healthEmpty.sprites[0]->sprite->setScale(sf::Vector2f((60 / 54), (340 / 336)));
-	healthEmpty.sprites[0]->width = 60;
-	healthEmpty.sprites[0]->height = 340;
+	//healthEmpty.sprites[0]->sprite->setScale(sf::Vector2f((60 / 54), (340 / 336)));
+	healthEmpty.sprites[0]->width = 144;
+	healthEmpty.sprites[0]->height = 124;
+	healthEmpty.globalSpeed = 0.02;
 	animations.animations["healthEmpty"] = healthEmpty;
 
 
-	animations.SwitchAnimations("healthEmpty");
+	animations.SwitchAnimations("healthFull");
 
 	collider = new BoxCollider();
 	collider->size = sf::Vector2f(animations.curSprite->width, animations.curSprite->height);
@@ -41,5 +44,5 @@ Health::~Health()
 
 void Health::Update(float dt)
 {
-
+	animations.Update(dt);
 }
