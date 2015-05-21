03fbe46b6614ad6f5bb86c3b4d5e0314da683b04
CSchnitzerling <chris.schnitzerling@hotmail.com>
2015-05-21 12:05:19 +1000

    Health Icons

--- GreenBanana/Health.cpp
+++ GreenBanana/Health.cpp
@@ -9,15 +9,26 @@ Health::Health(std::string type, std::string name, float x, float y, bool worldS
 	SetIsStatic(isStatic);
 	SetDepth(depth);
 
-	Animation idle;
 	SpritesheetLoader loader;
-	idle.sprites = loader.Load("Resources/Objects/CandyCaneBroke.png", 54, 336, 1);
-	idle.endEvent = Animation::AnimationEndEvent::HoldLastFrame;
-	idle.sprites[0]->sprite->setScale(sf::Vector2f((60 / 54), (340 / 336)));
-	idle.sprites[0]->width = 60;
-	idle.sprites[0]->height = 340;
-	animations.animations["idle"] = idle;
-	animations.SwitchAnimations("idle");
+
+	Animation healthFull;
+	healthFull.sprites = loader.Load("Resources/Objects/HealthObjects/sugar.png", 54, 336, 89);
+	healthFull.endEvent = Animation::AnimationEndEvent::HoldLastFrame;
+	healthFull.sprites[0]->sprite->setScale(sf::Vector2f((60 / 54), (340 / 336)));
+	healthFull.sprites[0]->width = 60;
+	healthFull.sprites[0]->height = 340;
+	animations.animations["healthFull"] = healthFull;
+
+	Animation healthEmpty;
+	healthEmpty.sprites = loader.Load("Resources/Objects/HealthObjects/sugar_Empty.png", 54, 336, 1);
+	healthEmpty.endEvent = Animation::AnimationEndEvent::HoldLastFrame;
+	healthEmpty.sprites[0]->sprite->setScale(sf::Vector2f((60 / 54), (340 / 336)));
+	healthEmpty.sprites[0]->width = 60;
+	healthEmpty.sprites[0]->height = 340;
+	animations.animations["healthEmpty"] = healthEmpty;
+
+
+	animations.SwitchAnimations("healthEmpty");
 
 	collider = new BoxCollider();
 	collider->size = sf::Vector2f(animations.curSprite->width, animations.curSprite->height);
